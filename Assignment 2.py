58c8440ca3fc12a43738494d101a4c8532c92afe
shee12rsh <157008875+shee12rsh@users.noreply.github.com>
2025-08-06 13:02:57 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,13 @@
+#Task 1
+num1 = input("Enter the number:")
+num1 = int(num1)
+if num1 % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
+#Task 2
+
+num = 0
+for i in range(1,51):
+    num = num + i
+print(num)
